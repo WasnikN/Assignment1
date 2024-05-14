2178759a7a9d53d50ab33ed111af888c4757127e
Nikhil Wasnik <nikhil.wasnik@bp.com>
2024-05-14 09:24:15 +0530

    added factorial class

---
+++ Assignment1/Assignments/Factorial.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace Assignment1.Assignments
+{
+    public class Factorial
+    {
+        public BigInteger Calculate(BigInteger number)
+        {
+            BigInteger result = 1;
+
+            for (BigInteger i = 2; i <= number; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
